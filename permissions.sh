e839057cb575e9687d2f15813e608da0b8dd33ec
gowtham <gowtham1.ait@gmail.com>
2024-09-17 19:12:02 +0530

    Update permissions.sh

--- permissions.sh
+++ permissions.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sudo chmod +x /opt/nodejs/PCAI-Frontend/server_start.sh
-sudo chmod +x /opt/nodejs/PCAI-Frontend/before_install.sh
-sudo chmod +x /opt/nodejs/PCAI-Frontend/permissions.sh
+sudo chmod +x /home/ubuntu/test/server_start.sh
+sudo chmod +x /home/ubuntu/test/before_install.sh
+sudo chmod +x /home/ubuntu/test/permissions.sh
