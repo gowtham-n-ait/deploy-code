b99c67515025db9942088a30159e64aaecf5e2f2
gowtham <gowtham1.ait@gmail.com>
2024-09-17 16:31:42 +0530

    Update permissions.sh

--- awsdeploy/permissions.sh
+++ awsdeploy/permissions.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sudo chmod +x /opt/nodejs/pcai/awsdeploy/server_start.sh
-sudo chmod +x /opt/nodejs/pcai/awsdeploy/before_install.sh
-sudo chmod +x /opt/nodejs/pcai/awsdeploy/permissions.sh
+sudo chmod +x /opt/nodejs/PCAI-Frontend/awsdeploy/server_start.sh
+sudo chmod +x /opt/nodejs/PCAI-Frontend/awsdeploy/before_install.sh
+sudo chmod +x /opt/nodejs/PCAI-Frontend/awsdeploy/permissions.sh
