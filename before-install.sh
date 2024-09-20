8c1ba3149b9172a075f16aad51c51801690daac8
gowtham <gowtham1.ait@gmail.com>
2024-09-20 18:36:57 +0530

    Update before-install.sh

--- before-install.sh
+++ before-install.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 # deleting the content of test directory
-sudo rm -rf /opt/sample/test
-sudo mkdir /opt/sample/test
-sudo chown -R dev:dev /opt/sample/test
+sudo rm -rf /opt/sample/test/*
+#sudo mkdir /opt/sample/test
